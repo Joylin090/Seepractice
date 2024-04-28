e7a0fdb6c49bb6c822deef1ffd725929a91e6c7f
Joylin D'Cunha <joylindcunha909@gmail.com>
2024-04-28 10:56:20 +0530

    commit1

---
+++ CasetoolsExam/src/practice/vehicle_203.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class vehicle_203 {
+	String brand;
+	int speed;
+	
+	vehicle_203(String brand){
+		this.brand=brand;
+		this.speed=0;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
